3a50a572f6e3b3ec1cfe117833f83c158c833f97
Spirt007 <37541698+Spirt007@users.noreply.github.com>
2019-03-29 12:05:26 +0300

    Add files via upload

---
+++ arch1.1.sh
@@ -0,0 +1,30 @@
+﻿#!/bin/bash
+loadkeys ru
+setfont cyr-sun16
+timedatectl set-ntp true
+
+echo 'Форматирование'
+pacman -Sy --noconfirm
+#mkfs.ext4 /dev/sda4 -L home
+mkfs.ext4 /dev/sda1 -L root
+mkswap /dev/sda3 -L swap
+mkfs.ext4 /dev/sda2 -L Linux
+
+echo 'Монтирование'
+echo
+mount /dev/sda1 /mnt
+mkdir /mnt/home
+mkdir /mnt/kali
+mount /dev/sda4 /mnt/home
+mount /dev/sda2 /mnt/Linux
+swapon /dev/sda3
+echo
+echo 'Выбор зеркал для загрузки. Ставим зеркало от Яндекс'
+echo "Server = http://mirror.yandex.ru/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
+
+echo 'Установка основных пакетов'
+pacstrap /mnt base base-devel
+
+echo 'Настройка системы'
+genfstab -pU /mnt >> /mnt/etc/fstab
+
