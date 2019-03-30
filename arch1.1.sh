92acb4ecd92e2a351f830f45f1853cbabfc998fd
Spirt007 <otstawnoi-stepan@mail.ru>
2019-03-30 20:38:01 +0300

    Update arch1.1.sh

--- arch1.1.sh
+++ arch1.1.sh
@@ -2,17 +2,13 @@
 loadkeys ru
 setfont cyr-sun16
 timedatectl set-ntp true
-
 echo 'Форматирование'
-echo
 pacman -Sy --noconfirm
 mkfs.ext4 /dev/sda4 -L home
 mkfs.ext4 /dev/sda1 -L root
 mkswap /dev/sda3 -L swap
 mkfs.ext4 /dev/sda2 -L Linux
-
 echo 'Монтирование'
-echo
 mount /dev/sda1 /mnt
 mkdir /mnt/home
 mkdir /mnt/Linux
@@ -21,11 +17,8 @@ mount /dev/sda4 /mnt/home
 mount /dev/sda2 /mnt/Linux
 mount /dev/sdb5 /mnt/Stock
 swapon /dev/sda3
-
 echo 'Установка основных пакетов'
 pacstrap /mnt base base-devel
-
 echo 'Настройка системы'
 genfstab -pU /mnt >> /mnt/etc/fstab
-
 arch-chroot /mnt sh -c "$(curl -fsSL  spirt007.github.io/Install-Arch/arch1.2.sh)"
