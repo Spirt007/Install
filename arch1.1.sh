ae82a7185c8f887fbcea4732f9d07dd5a72aaebd
Spirt007 <otstawnoi-stepan@mail.ru>
2019-04-01 18:58:54 +0300

    Add files via upload

--- arch1.1.sh
+++ arch1.1.sh
@@ -8,14 +8,12 @@ mkfs.ext4 /dev/sda1 -L root
 mkswap /dev/sda3 -L swap
 mkfs.ext4 /dev/sda2 -L Linux
 mount /dev/sda1 /mnt
-mkdir /mnt/home
-mkdir /mnt/Linux
-mkdir /mnt/Stock
+mkdir /mnt{home,Linux,Stock}
 mount /dev/sda4 /mnt/home
 mount /dev/sda2 /mnt/Linux
 mount /dev/sdb5 /mnt/Stock
 swapon /dev/sda3
-echo "Server = http://mirror.yandex.ru/archlinux/$repo/os/$arch" > /etc/pacman.d/mirrorlist
+echo "Server = http://mirror.yandex.ru/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
 pacstrap /mnt base base-devel
 genfstab -pU /mnt >> /mnt/etc/fstab
 arch-chroot /mnt sh -c "$(curl -fsSL  spirt007.github.io/Install-Arch/arch1.2.sh)"
