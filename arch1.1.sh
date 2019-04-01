8871319833874ae9fc1360c079de776cc56f750e
Spirt007 <otstawnoi-stepan@mail.ru>
2019-04-01 23:46:31 +0300

    Update arch1.1.sh

--- arch1.1.sh
+++ arch1.1.sh
@@ -6,8 +6,8 @@ pacman -Sy --noconfirm
 mkfs.ext4 /dev/sda1 -L root
 mkfs.ext4 /dev/sda2 -L linux
 mkswap /dev/sda3 -L swap
-mkfs.ext4 dev/sda4 -L home
-
+#mkfs.ext4 dev/sda4 -L home
+echo
 mount /dev/sda1 /mnt
 mkdir /mnt/home
 mkdir /mnt/linux
@@ -16,9 +16,9 @@ mount /dev/sda2 /mnt/linux
 mount /dev/sda4 /mnt/home
 mount /dev/sdb5 /mnt/stock
 swapon /dev/sda3
-
+echo
 echo "Server = http://mirror.yandex.ru/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
-
+echo
 pacstrap /mnt base base-devel
 genfstab -pU /mnt >> /mnt/etc/fstab
 arch-chroot /mnt sh -c "$(curl -fsSL  spirt007.github.io/Install-Arch/arch1.2.sh)"
