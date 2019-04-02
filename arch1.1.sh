e211f4275e654c522df32921e503056d43612965
Spirt007 <otstawnoi-stepan@mail.ru>
2019-04-02 16:26:19 +0300

    Add files via upload

--- arch1.1.sh
+++ arch1.1.sh
@@ -1,21 +1,67 @@
-﻿#!/bin/bash
+#!/bin/bash
+
+# Arch Linux Fast Install - Быстрая установка Arch Linux https://github.com/ordanax/arch2018
+# Цель скрипта - быстрое развертывание системы с вашими персональными настройками (конфиг XFCE, темы, программы и т.д.).
+
+# В разработке принимали участие:
+# Алексей Бойко https://vk.com/ordanax
+# Степан Скрябин https://vk.com/zurg3
+# Михаил Сарвилин https://vk.com/michael170707
+# Данил Антошкин https://vk.com/danil.antoshkin
+
 loadkeys ru
 setfont cyr-sun16
+echo 'Скрипт сделан на основе чеклиста Бойко Алексея по Установке ArchLinux'
+echo 'Ссылка на чек лист есть в группе vk.com/arch4u'
+
+echo '2.3 Синхронизация системных часов'
 timedatectl set-ntp true
 
-echo 'Разметка диска'
+echo '2.4 создание разделов'
+(
+  echo o;
+
+  echo n;
+  echo;
+  echo;
+  echo;
+  echo +20G;
+
+  echo n;
+  echo;
+  echo;
+  echo;
+  echo +20G;
+
+  echo n;
+  echo;
+  echo;
+  echo;
+  echo +8G;
+
+  echo n;
+  echo p;
+  echo;
+  echo;
+  echo a;
+  echo 1;
+
+  echo w;
+) | fdisk /dev/sda
+
+echo 'Ваша разметка диска'
 fdisk -l
 
 echo '2.4.2 Форматирование дисков'
-mkfs.ext4 /dev/sda1 -L root
-mkfs.ext4 /dev/sda2 -L linux
+mkfs.ext2  /dev/sda1 -L linux
+mkfs.ext4  /dev/sda2 -L root
 mkswap /dev/sda3 -L swap
-mkfs.ext4 dev/sda4 -L home
+mkfs.ext4  /dev/sda4 -L home
 
 echo '2.4.3 Монтирование дисков'
-mount /dev/sda1 /mnt
-mkdir /mnt/{linux,home,stock}
-mount /dev/sda2 /mnt/linux
+mount /dev/sda2 /mnt
+mkdir /mnt/{root,linux,stock}
+mount /dev/sda1 /mnt/linux
 swapon /dev/sda3
 mount /dev/sda4 /mnt/home
 mount /dev/sdb5 /mnt/stock
@@ -24,9 +70,9 @@ echo '3.1 Выбор зеркал для загрузки. Ставим зерк
 echo "Server = http://mirror.yandex.ru/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
 
 echo '3.2 Установка основных пакетов'
-pacstarp /mnt base base-devel
+pacstrap /mnt base base-devel
 
 echo '3.3 Настройка системы'
 genfstab -pU /mnt >> /mnt/etc/fstab
 
-arch-chroot /mnt sh -c "$(curl -fsSL  spirt007.github.io/Install-Arch/arch1.2.sh)"
+arch-chroot /mnt sh -c "$(curl -fsSL git.io/arch2.sh)"
