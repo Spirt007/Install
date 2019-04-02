81cdaf74c33285ab66ab9a02a75bf6beeb929d76
Spirt007 <otstawnoi-stepan@mail.ru>
2019-04-02 13:29:19 +0300

    Update arch1.1.sh

--- arch1.1.sh
+++ arch1.1.sh
@@ -2,20 +2,30 @@
 loadkeys ru
 setfont cyr-sun16
 timedatectl set-ntp true
-pacman -Sy --noconfirm
+
+echo 'Разметка диска'
+fdisk -l
+
+echo '2.4.2 Форматирование дисков'
 mkfs.ext4 /dev/sda1 -L root
 mkfs.ext4 /dev/sda2 -L linux
 mkswap /dev/sda3 -L swap
 mkfs.ext4 dev/sda4 -L home
-mount /dev/sda1 /mnt
-mkdir /mnt/home
-mkdir /mnt/linux
 
+echo '2.4.3 Монтирование дисков'
+mount /dev/sda1 /mnt
+mkdir /mnt/{linux,home,stock}
 mount /dev/sda2 /mnt/linux
 mount /dev/sda4 /mnt/home
-
 swapon /dev/sda3
+
+echo '3.1 Выбор зеркал для загрузки. Ставим зеркало от Яндекс'
 echo "Server = http://mirror.yandex.ru/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
+
+echo '3.2 Установка основных пакетов'
 pacstarp /mnt base base-devel
+
+echo '3.3 Настройка системы'
 genfstab -pU /mnt >> /mnt/etc/fstab
+
 arch-chroot /mnt sh -c "$(curl -fsSL  spirt007.github.io/Install-Arch/arch1.2.sh)"
