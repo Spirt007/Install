b232241d6621c942b036a2de778526ad1f912449
Spirt007 <otstawnoi-stepan@mail.ru>
2019-04-04 19:06:57 +0300

    Add files via upload

--- arch1.1.sh
+++ arch1.1.sh
@@ -60,14 +60,14 @@ mkfs.ext4  /dev/sda4 -L home
 
 echo '2.4.3 Монтирование дисков'
 mount /dev/sda2 /mnt
-mkdir /mnt/{root,linux,home,stock}
+mkdir /mnt/{root,linux,stock}
 mount /dev/sda1 /mnt/linux
 swapon /dev/sda3
 mount /dev/sda4 /mnt/home
 mount /dev/sdb5 /mnt/stock
 
 echo '3.1 Выбор зеркал для загрузки. Ставим зеркало от Яндекс'
-
+echo "Server = http://mirror.yandex.ru/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
 
 echo '3.2 Установка основных пакетов'
 pacstrap /mnt base base-devel
@@ -75,4 +75,4 @@ pacstrap /mnt base base-devel
 echo '3.3 Настройка системы'
 genfstab -pU /mnt >> /mnt/etc/fstab
 
-arch-chroot /mnt sh -c "$(curl -fsSL git.io/arch2.sh)"
+arch-chroot /mnt sh -c "$(curl -fsSL spirt007.github.io/arch1.2.sh)"
