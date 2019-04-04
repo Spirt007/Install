5ae5a8364b2e758eeec462023a8f0427d1dda06b
Spirt007 <otstawnoi-stepan@mail.ru>
2019-04-04 09:23:49 +0300

    Add files via upload

--- arch1.3.sh
+++ arch1.3.sh
@@ -7,24 +7,24 @@ echo 'Установка AUR (yay)'
 sudo pacman -Syu
 sudo pacman -S wget --noconfirm
 wget git.io/yay-install.sh && sh yay-install.sh --noconfirm
+
+echo 'Создаем нужные директории'
 sudo pacman -S xdg-user-dirs --noconfirm
 xdg-user-dirs-update
-sudo pacman -S firefox ufw qt4 f2fs-tools dosfstools ntfs-3g alsa-lib alsa-utils file-roller p7zip unrar gvfs aspell-ru pulseaudio --noconfirm
 
-sudo pacman -S obs-studio veracrypt freemind filezilla gimp libreoffice conky conky-manager libreoffice-fresh-ru kdenlive screenfetch vlc qbittorrent gnome-calculator --noconfirm
-yay -S timeshift flameshot-git xflux sublime-text-dev hunspell-ru pamac-aur --noconfirm  
-echo 'Установка тем'
-yay -S osx-arc-shadow papirus-maia-icon-theme-git breeze-default-cursor-theme --noconfirm
-sudo pacman -S capitaine-cursors
+echo 'Установка программ'
+sudo pacman -S firefox firefox-i18n-ru alsa-lib alsa-utils os-prober file-roller p7zip unrar gvfs aspell-ru pulseaudio gimp libreoffice libreoffice-fresh-ru 
+filezilla kdenlive screenfetch vlc pidgin transmission-gtk transmission-cli djview4 gparted gnome-calculator conky conky-manager --noconfirm
+  yay -S pamac-aur gksu flameshot-git sublime-text-dev sddm-config-editor-git --noconfirm
+
+  echo 'Установка тем'
+  yay -S osx-arc-shadow papirus-maia-icon-theme-git breeze-default-cursor-theme --noconfirm
+  sudo pacman -S capitaine-cursors
   
-wget git.io/arch_logo.png
-sudo mv -f ~/Downloads/arch_logo.png /usr/share/pixmaps/arch_logo.png
+  echo 'Ставим лого ArchLinux в меню'
+  wget git.io/arch_logo.png
+  sudo mv -f ~/Downloads/arch_logo.png /usr/share/pixmaps/arch_logo.png
 
-echo 'Включаем сетевой экран'
-sudo ufw enable
-
-echo 'Добавляем в автозагрузку:'
-sudo systemctl enable ufw
 
 sudo rm -rf ~/Downloads
 sudo rm -rf ~/arch3.sh
