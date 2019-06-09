e751a66dc1fb7336e5428fdb62ea2804aad6daf2
Spirt007 <otstawnoi-stepan@mail.ru>
2019-06-09 21:28:38 +0300

    Update arch1.1.sh

--- arch1.1.sh
+++ arch1.1.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Arch Linux Fast Install - Быстрая установка Arch Linux https://github.com/ordanax/arch2018
-# Цель скрипта - быстрое развертывание системы с вашими персональными настройками (конфиг XFCE, темы, программы и т.д.).
-
-# В разработке принимали участие:
-# Алексей Бойко https://vk.com/ordanax
-# Степан Скрябин https://vk.com/zurg3
-# Михаил Сарвилин https://vk.com/michael170707
-# Данил Антошкин https://vk.com/danil.antoshkin
 
 loadkeys ru
 setfont cyr-sun16
