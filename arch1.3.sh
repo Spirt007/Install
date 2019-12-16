41f8df6f26e17e080de16d217c91efac01173616
Spirt007 <otstawnoi-stepan@mail.ru>
2019-12-17 00:02:12 +0300

    Update arch1.3.sh

--- arch1.3.sh
+++ arch1.3.sh
@@ -12,7 +12,7 @@ sudo pacman -S xdg-user-dirs --noconfirm
 xdg-user-dirs-update
 
 echo 'Установка программ'
-sudo pacman -S firefox firefox-i18n-ru alsa-lib alsa-utils ttf-liberation ttf-dejavu bash-completion gpm opendesktop-fonts ttf-bitstream-vera ttf-arphic-ukai ttf-arphic-uming ttf-hanazono lib32-mesa-libgl os-prober doublecmd-gtk2 file-roller p7zip unrar gvfs aspell-ru pulseaudio gimp libreoffice libreoffice-fresh-ru filezilla kdenlive screenfetch vlc pidgin transmission-gtk transmission-cli djview4 gparted gnome-calculator conky conky-manager leafpad Thunderbird --noconfirm 
+sudo pacman -S firefox firefox-i18n-ru alsa-lib alsa-utils ttf-liberation ttf-dejavu bash-completion gpm opendesktop-fonts ttf-bitstream-vera ttf-arphic-ukai ttf-arphic-uming ttf-hanazono lib32-mesa-libgl os-prober doublecmd-gtk2 file-roller p7zip unrar gvfs aspell-ru pulseaudio gimp libreoffice libreoffice-fresh-ru filezilla kdenlive screenfetch vlc pidgin transmission-gtk transmission-cli djview4 gparted gnome-calculator conky conky-manager leafpad thunderbird gpicview --noconfirm 
 yay -S pamac-aur gksu sublime-text-dev downgrade sddm-config-editor-git zenity pidgin-mrim-prpl --noconfirm
 
 echo 'Установка тем'
