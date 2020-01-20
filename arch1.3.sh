9d3a1076ca19ed126f551b41c709ae2601f0d423
Spirt007 <otstawnoi-stepan@mail.ru>
2020-01-20 15:38:36 +0300

    Update arch1.3.sh

--- arch1.3.sh
+++ arch1.3.sh
@@ -12,7 +12,7 @@ sudo pacman -S xdg-user-dirs --noconfirm
 xdg-user-dirs-update
 
 echo 'Установка программ'
-sudo pacman -S pavucontrol youtube-dl telegram-desktop f2fs-tools ntfs-3g firefox firefox-i18n-ru alsa-lib alsa-utils ttf-liberation ttf-dejavu bash-completion gpm opendesktop-fonts ttf-bitstream-vera ttf-arphic-ukai ttf-arphic-uming ttf-hanazono lib32-mesa-libgl os-prober doublecmd-gtk2 file-roller p7zip unrar gvfs aspell-ru pulseaudio gimp filezilla kdenlive screenfetch vlc pidgin transmission-gtk transmission-cli djview4 gparted gnome-calculator conky conky-manager leafpad thunderbird gpicview steam --noconfirm 
+sudo pacman -S pavucontrol  ffmpegthumbnailer raw-thumbnailer youtube-dl telegram-desktop f2fs-tools ntfs-3g firefox firefox-i18n-ru alsa-lib alsa-utils ttf-liberation ttf-dejavu bash-completion gpm opendesktop-fonts ttf-bitstream-vera ttf-arphic-ukai ttf-arphic-uming ttf-hanazono lib32-mesa-libgl os-prober doublecmd-gtk2 file-roller p7zip unrar gvfs aspell-ru pulseaudio gimp filezilla kdenlive screenfetch vlc pidgin transmission-gtk transmission-cli djview4 gparted gnome-calculator conky conky-manager leafpad thunderbird gpicview steam --noconfirm 
 yay -S pamac-aur gksu sublime-text-dev downgrade sddm-config-editor-git zenity pidgin-mrim-prpl hunspell-ru --noconfirm
 
 echo 'Установка тем'
